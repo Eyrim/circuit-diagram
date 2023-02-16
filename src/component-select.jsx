2f342a7d28d0f05a1e0e3a909b3d8c334a967bae
Eyrim <marfxedbot@gmail.com>
2023-02-16 22:40:36 +0000

    can now render component images in component-select, callback in app.jsx passed as prop

--- src/component-select.jsx
+++ src/component-select.jsx
@@ -1,1 +1,22 @@
 import React from "react";
+
+import "./styling/component-select.css";
+
+export default class ComponentSelect extends React.Component {
+    constructor(handleClick) {
+        super(handleClick);
+        
+        this.state = {
+            arr: [
+                <img src="./images/circuit-symbols/resistor-nema.png" onClick={this.props.handleClick} key="nema-resistor" data-index="nema-resistor"/>, 
+                <img src="./images/circuit-symbols/resistor-iec.png" onClick={this.props.handleClick} key="iec-resistor" data-index="iec-resistor"/>
+            ]
+        }
+    }
+
+    render() {
+        return <div className="componentSelect">
+            <p>{this.state.arr}</p>
+        </div>;
+    }
+}
--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "../styling/app.css";
 import DiagramArea from "../diagram-area";
+import ComponentSelect from "../component-select";
 
 /**
  * 
@@ -10,12 +11,27 @@ export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            selectedComponent: {},
-        }
+            selectedComponentName: "a",
+        };
+
+        this.handleClick = this.handleClick.bind(this);
     }
 
     render() {
-        return <DiagramArea>
-        </DiagramArea>;      
+        return <div> 
+            <ComponentSelect handleClick={this.handleClick}>
+            </ComponentSelect>
+            {this.state.selectedComponentName}
+        </div>;      
+    }
+
+    handleClick(e) {
+        let clickedComponentName = e.target.getAttribute("data-index");
+
+        console.log(clickedComponentName);
+
+        this.setState({
+            selectedComponentName: clickedComponentName
+        });
     }
 }
