9193b8c710c0e3234df65170abe777b6453e3d4f
Eyrim <marfxedbot@gmail.com>
2023-03-10 18:27:29 +0000

    Component.jsx converted into a function instead of a class

--- src/component.jsx
+++ src/component.jsx
@@ -1,29 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
+// Electrical Component
+export default function Component(props) {
+    const [xPos, setXPos] = useState(-1);
+    const [yPos, setYPos] = useState(-1);
+    const [symbolPath, setSymbolPath] = useState("");
+    const [componentName, setComponentName] = useState("");
 
-// Electrical component
-export default class Component extends React.Component  {
-    constructor(xPos, yPos, symbolpath, componentName) {
-        super();
+    // Ran on mount
+    //TODO: this is running 2 times, shouldn't be an issue but inefficent
+    useEffect(() => {
+        setXPos(props.xPos);
+        setYPos(props.yPos);
+        setSymbolPath(props.symbolPath);
+        setComponentName(props.componentName);
 
-        this.state = {
-            xPos: 0,
-            yPos: 0,
-            symbolPath: "",
-            componentName: "",
-        };
-    }
+    }, [props, xPos, yPos])
 
-    componentDidMount() {
-        this.setState({
-            xPos: this.props.xPos,
-            yPo: this.props.yPos,
-            symbolPath: this.props.symbolPath,
-            componentName: this.props.componentName
-        });
-    }
-
-    render() {
-        return <img src={this.state.symbolPath} alt={this.state.componentName}/>;
-    }
+    return <img src={symbolPath} alt={componentName}/>;
 }
