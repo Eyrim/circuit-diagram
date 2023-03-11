44a3cfc335395c908e1725da8c729568fb772edf
Eyrim <marfxedbot@gmail.com>
2023-03-11 20:02:23 +0000

    Converted root.jsx into a function component

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-export default class Root extends React.Component {
-    render() {
-        return <div>
+
+export default function Root(props) {
+
+    function launchAppButton() {
+        // Send the user to the app
+        window.location.href = window.location.href += "app";
+    }
+
+    return <div>
             <u><h1>This project is based off of an <a href="https://github.com/Eyrim/circuit-sharp">old project</a> I originally wrote in C#, and is mostly done for my own educational purposes<br/>
             <strong>THERE ARE BETTER CIRCUIT DIAGRAM SOFTWARE OUT THERE:<br/></strong>
             This is my favourite: <a href="https://www.circuit-diagram.org/">circuit-diagram.org</a></h1></u>
             <br/>
             <br/>
-            Following that, press <button type="button" onClick={this.#launchAppButton}>here</button> to launch the application
+            Following that, press <button type="button" onClick={launchAppButton}>here</button> to launch the application
         </div>;
-    }
-
-    #launchAppButton() {
-        // Redirect the user to the current url + "/app"
-        window.location.href = window.location.href += "app";
-    }
 }
